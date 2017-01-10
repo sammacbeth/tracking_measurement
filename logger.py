06bbe6775dc47192ed76d51d09cf2f71c865f2d7
Sam Macbeth <sam@cliqz.com>
2017-01-10 10:54:58 +0100

    specify log dir in logger

--- logger.py
+++ logger.py
@@ -1,5 +1,7 @@
 import fileinput
 import json
+import argparse
+import sys
 from logging.handlers import TimedRotatingFileHandler
 
 class NoFormatter():
@@ -8,11 +10,14 @@ class NoFormatter():
         return record.strip()
 
 if __name__ == '__main__':
-    logger = TimedRotatingFileHandler('reqlog.jl', when='h', utc=True)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("logdir")
+    args = parser.parse_args()
+    logger = TimedRotatingFileHandler(args.logdir + '/reqlog.jl', when='h', utc=True)
     formatter = NoFormatter()
     logger.setFormatter(formatter)
 
-    for line in fileinput.input():
+    for line in sys.stdin:
         logger.emit(line)
 
     logger.flush()
